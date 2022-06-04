1a4b8a1f02689179a48c50b34ee9efcbe47c3327
Chris Roberts <code@chrisroberts.org>
2022-06-04 06:51:17 -0700

    Configure egon with xmodmap

--- machine/egon.nix
+++ machine/egon.nix
@@ -3,4 +3,19 @@
 {
   imports =
     [ ../user/spox.nix ../role/workstation.nix ../context/personal.nix ];
+
+  systemd.user.services = {
+    xmodmap = {
+      Unit = {
+        Description = "Cap key reconfigure";
+        Requires = "graphical-session.target";
+      };
+      Service = {
+        ExecStart = "xmodmap ${config.xdg.cacheHome}/xmodmap";
+        Restart = "never";
+      };
+    };
+  };
+
+  xdg.configFile = { "xmodmap".source = ../config/caps-xmodmap; };
 }
