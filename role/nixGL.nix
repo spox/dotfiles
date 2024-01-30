1ff526de0591a4dcbf4b806621c5f5a87bb24de5
spox <code@chrisroberts.org>
2024-01-30 08:00:25 -0800

    Update the nixgl wrapper

--- role/nixGL.nix
+++ role/nixGL.nix
@@ -9,7 +9,12 @@ in pkgs.buildEnv {
   name = "nixGL-${pkg.name}";
   paths = [ pkg ] ++ (map (bin:
     lib.hiPrio (pkgs.writeShellScriptBin bin ''
-      exec -a "$0" "${nixGL.auto.nixGLDefault}/bin/nixGL" "${bins}/${bin}" "$@"
+      . "${nixGL.auto.nixGLDefault}/bin/nixGL"
+      export -n LIBGL_DRIVERS_PATH
+      export -n LIBVA_DRIVERS_PATH
+      export -n __EGL_VENDOR_LIBRARY_FILENAMES
+      export -n LD_LIBRARY_PATH
+      exec -a "$0" "${bins}/${bin}" "$@"
     '')) (builtins.attrNames (builtins.readDir bins)));
   meta.priority = 1;
 }
