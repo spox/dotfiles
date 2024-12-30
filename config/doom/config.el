d95ee4d6e7ffc1e3b4022fff5de3c3ec209f532e
Chris Roberts <croberts@hashicorp.com>
2024-12-29 16:10:40 -0800

    Update configs

--- config/doom/config.el
+++ config/doom/config.el
@@ -4,14 +4,14 @@
   (doom/set-indent-width 2))
 (add-hook 'ruby-mode-hook 'ruby-mode-indent)
 
-(setq-default doom-theme 'doom-gruvbox)
-(defun load-doom-gruvbox-theme (frame)
+(setq-default doom-theme 'doom-xcode)
+(defun load-doom-xcode-theme (frame)
   (select-frame frame)
-  (load-theme 'doom-gruvbox t))
+  (load-theme 'doom-xcode t))
 
 (if (daemonp)
-    (add-hook 'after-make-frame-functions #'load-doom-gruvbox-theme)
-  (load-theme 'doom-gruvbox t))
+    (add-hook 'after-make-frame-functions #'load-doom-xcode-theme)
+  (load-theme 'doom-xcode t))
 
 ;; (add-to-list 'auto-mode-alist
 ;;              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
