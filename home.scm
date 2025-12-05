4464b9234fd8400fc448fa32b8dbb6ecb4e4763e
Chris Roberts <code@chrisroberts.org>
2025-12-05 11:31:27 -0800

    locale updates

--- home.scm
+++ home.scm
@@ -18,117 +18,120 @@
 (home-environment
  (packages
   (specifications->packages
-   (list "kitty" ;; shell things ;;
-         "tmux"
-         "tmux-themepack"
-         "starship"
-         ;; language things ;;
-         "go"
-         "protobuf"
-         ;; shell utilities ;;
-         "awscli@2"
-         "bottom"
-         "btop"
-         ;; "caddy"
-         "catimg"
-         "direnv"
-         "dos2unix"
-         "du-dust"
-         "eza"
-         "ffmpeg"
-         "glances"
-         "htop"
-         "imagemagick"
-         "jless"
-         "jq"
-         "kismet"
-         "lm-sensors"
-         ;; mise
-         "netcat"
-         "nethogs"
-         "net-tools"
-         "nmap"
-         "7zip"
-         "procs"
-         "progress"
-         ;; "sshutle"
-         "unzip"
-         "zoxide"
-         ;; development related things ;;
-         "bat"
-         "bats"
-         "coreutils"
-         "git-delta"
-         "emacs"
-         "fd"
-         "git"
-         "gopls"
-         "graphviz"
-         "imhex"
-         "jj-vcs"
-         "ripgrep"
-         "shellcheck"
-         "tig"
-         ;; shell usability things ;;
-         "croc" ; file sharing
-         "neofetch" ; system info
-         "qrencode" ; qrcode generator
-         "whois" ; dns info lookups
-         "playerctl" ; control audio via dbus
-         "brightnessctl" ; control backlight stuff
+   (list
+    "glibc-locales"
+     ;; shell things ;;
+    "kitty"
+    "tmux"
+    "tmux-themepack"
+    "starship"
+    ;; language things ;;
+    "go"
+    "protobuf"
+    ;; shell utilities ;;
+    "awscli@2"
+    "bottom"
+    "btop"
+    ;; "caddy"
+    "catimg"
+    "direnv"
+    "dos2unix"
+    "du-dust"
+    "eza"
+    "ffmpeg"
+    "glances"
+    "htop"
+    "imagemagick"
+    "jless"
+    "jq"
+    "kismet"
+    "lm-sensors"
+    ;; mise
+    "netcat"
+    "nethogs"
+    "net-tools"
+    "nmap"
+    "7zip"
+    "procs"
+    "progress"
+    ;; "sshutle"
+    "unzip"
+    "zoxide"
+    ;; development related things ;;
+    "bat"
+    "bats"
+    "coreutils"
+    "git-delta"
+    "emacs"
+    "fd"
+    "git"
+    "gopls"
+    "graphviz"
+    "imhex"
+    "jj-vcs"
+    "ripgrep"
+    "shellcheck"
+    "tig"
+    ;; shell usability things ;;
+    "croc" ; file sharing
+    "neofetch" ; system info
+    "qrencode" ; qrcode generator
+    "whois" ; dns info lookups
+    "playerctl" ; control audio via dbus
+    "brightnessctl" ; control backlight stuff
 
-         ;; extra stuff ;;
-         "hunspell"
-         "hunspell-dict-en"
-         "hunspell-dict-en-us"
-         ;; "1password-gui" 
-         ;; "1password-cli"
-         "weechat"
-         "weechat-wee-slack"
-         ;; desktop stuff ;;
-         "keepassxc"
-         "krita"
-         "librewolf"
-         "ungoogled-chromium"
-         ;; desktop environment stuff
-         "swayfx"
-         "swayidle"
-         "swaylock"
-         "swaybg"
-         "waybar"
-         "wl-clipboard"
-         "rofi"
-         "fuzzel"
-         "grim"  
-         "wlsunset"                  ;; Night light
-         "bemenu"
-         "slurp"                     ;; screen area selection       
-         "dunst"                     ;; notifications
-         "pinentry"                  ;; prompt for pgp, ssh, ...
-         "pavucontrol"               ;; audio control
-         "pamixer"                   ;; keyboard audio volumne
-         "brightnessctl"             ;; keyboard display brightness
-         "qtwayland"
-         "swappy"            ;; snapshot and edit
-         "wf-recorder"             ;; Screen Recording
-         "bemoji"
-         "kanshi"   ;; hot pluggable screen config
-         "udiskie" ;; removable disk automounter
-         "lxqt-policykit" ;; polkit
-         "xsettingsd" ;; settings for x windows
-         "swaynotificationcenter" ;; toasts
+    ;; extra stuff ;;
+    "hunspell"
+    "hunspell-dict-en"
+    "hunspell-dict-en-us"
+    ;; "1password-gui" 
+    ;; "1password-cli"
+    "weechat"
+    "weechat-wee-slack"
+    ;; desktop stuff ;;
+    "keepassxc"
+    "krita"
+    "librewolf"
+    "ungoogled-chromium"
+    ;; desktop environment stuff
+    "swayfx"
+    "swayidle"
+    "swaylock"
+    "swaybg"
+    "waybar"
+    "wl-clipboard"
+    "rofi"
+    "fuzzel"
+    "grim"  
+    "wlsunset"                  ;; Night light
+    "bemenu"
+    "slurp"                     ;; screen area selection       
+    "dunst"                     ;; notifications
+    "pinentry"                  ;; prompt for pgp, ssh, ...
+    "pavucontrol"               ;; audio control
+    "pamixer"                   ;; keyboard audio volumne
+    "brightnessctl"             ;; keyboard display brightness
+    "qtwayland"
+    "swappy"            ;; snapshot and edit
+    "wf-recorder"             ;; Screen Recording
+    "bemoji"
+    "kanshi"   ;; hot pluggable screen config
+    "udiskie" ;; removable disk automounter
+    "lxqt-policykit" ;; polkit
+    "xsettingsd" ;; settings for x windows
+    "swaynotificationcenter" ;; toasts
 
-         "date"
-         "font-openmoji"
-         "font-google-noto"
-         "font-google-noto-emoji"
-         "font-ibm-plex"
-         "font-hack"
-         "font-jetbrains-mono"
-         "font-awesome"
-         "font-google-material-design-icons"
-         "font-fira-code"
-         )))
+    "date"
+    "font-openmoji"
+    "font-google-noto"
+    "font-google-noto-emoji"
+    "font-ibm-plex"
+    "font-hack"
+    "font-jetbrains-mono"
+    "font-awesome"
+    "font-google-material-design-icons"
+    "font-fira-code"
+    )))
  
  ;; Below is the list of Home services.  To search for available
  ;; services, run 'guix home search KEYWORD' in a terminal.
@@ -158,7 +161,10 @@
                                                    ("CLUTTER_BACKEND" . "wayland")
                                                    ;; Cursor theme
                                                    ("XCURSOR_THEME" . "Adwaita")
-                                                   ("XCURSOR_SIZE" . "24")))
+                                                   ("XCURSOR_SIZE" . "24")
+                                                   ("GUIX_LOCPATH" . "~/.guix-profile/lib/locale")
+                                                   ("SSL_CERT_FILE" . "/etc/ssl/certs/ca-certificates.crt")
+                                                   ("CA_CERT_BUNDLE" . "/etc/ssl/certs/ca-certificates.crt")))
                           (bashrc (list (local-file "config/bashrc")
                                         (local-file "config/bash-aliases/bundler.sh")
                                         (local-file "config/bash-aliases/copypasta.sh")
