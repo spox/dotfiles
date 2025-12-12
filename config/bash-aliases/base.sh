a41a75fe0560033f2c4cb9919db1da259ef10f2e
Chris Roberts <code@chrisroberts.org>
2025-12-12 08:27:34 -0800

    handful of fixes/updates

---
+++ config/bash-aliases/base.sh
@@ -0,0 +1,11 @@
+# shellcheck shell=bash
+alias grep="grep --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+
+alias ls="ls -p --color=auto"
+alias l="ls -F"
+alias la="ls -a"
+alias ll="ls -alF"
+
+alias edit="emacs -nw"
--- home.scm
+++ home.scm
@@ -5,6 +5,7 @@
              (guix profiles)
              (guix channels)
              (gnu home services)
+             (gnu home services sound)
              (gnu home services dotfiles)
              (gnu home services guix)
              (gnu home services ssh)
@@ -84,7 +85,7 @@
     "hunspell"
     "hunspell-dict-en"
     "hunspell-dict-en-us"
-    ;; "1password-gui" 
+    ;; "1password-gui"
     ;; "1password-cli"
     "weechat"
     "weechat-wee-slack"
@@ -97,15 +98,17 @@
     "swayfx"
     "swayidle"
     "swaylock"
+    "waylock"
     "swaybg"
     "waybar"
+    "clipman"
     "wl-clipboard"
     "rofi"
     "fuzzel"
-    "grim"  
+    "grim"
     "wlsunset"                  ;; Night light
     "bemenu"
-    "slurp"                     ;; screen area selection       
+    "slurp"                     ;; screen area selection
     "dunst"                     ;; notifications
     "pinentry"                  ;; prompt for pgp, ssh, ...
     "pavucontrol"               ;; audio control
@@ -120,6 +123,9 @@
     "lxqt-policykit" ;; polkit
     "xsettingsd" ;; settings for x windows
     "swaynotificationcenter" ;; toasts
+    "blueman" ;; bluetooth manager
+
+
 
     "date"
     "font-openmoji"
@@ -132,40 +138,14 @@
     "font-google-material-design-icons"
     "font-fira-code"
     )))
- 
+
  ;; Below is the list of Home services.  To search for available
  ;; services, run 'guix home search KEYWORD' in a terminal.
  (services
   (append (list (service home-bash-service-type
                          (home-bash-configuration
-                          (aliases '(("grep" . "grep --color=auto")
-                                     ("ll" . "ls -l")
-                                     ("ls" . "ls -p --color=auto")
-                                     ("edit" . "emacs -nw")
-                                     ("egrep" . "egrep --color=auto")
-                                     ("fgrep" . "fgrep --color=auto")
-                                     ("l" . "ls -F")
-                                     ("la" . "ls -a")
-                                     ("ll" . "ls -alF")
-                                     ("ls" . "eza --color=auto --icons")))
-                          (environment-variables '(("GOBIN" . "~/.local/bin")
-                                                   ("GOPATH" . "~/.local/go")
-                                                   ("QT_QPA_PLATFORM" . "wayland;xcb")
-                                                   ("SDL_VIDEODRIVER" . "wayland")
-                                                   ("XDG_CURRENT_DESKTOP" . "sway")
-                                                   ("XDG_SESSION_DESKTOP" . "sway")
-                                                   ("XDG_SESSION_TYPE" . "wayland")
-                                                   ;; Performance environment variables for Wayland
-                                                   ("MOZ_ENABLE_WAYLAND" . "1")
-                                                   ("GDK_BACKEND" . "wayland")
-                                                   ("CLUTTER_BACKEND" . "wayland")
-                                                   ;; Cursor theme
-                                                   ("XCURSOR_THEME" . "Adwaita")
-                                                   ("XCURSOR_SIZE" . "24")
-                                                   ("GUIX_LOCPATH" . "~/.guix-profile/lib/locale")
-                                                   ("SSL_CERT_FILE" . "/etc/ssl/certs/ca-certificates.crt")
-                                                   ("CA_CERT_BUNDLE" . "/etc/ssl/certs/ca-certificates.crt")))
                           (bashrc (list (local-file "config/bashrc")
+                                        (local-file "config/bash-aliases/default.sh")
                                         (local-file "config/bash-aliases/bundler.sh")
                                         (local-file "config/bash-aliases/copypasta.sh")
                                         (local-file "config/bash-aliases/git.sh")
@@ -174,20 +154,32 @@
                 (service home-dotfiles-service-type
                          (home-dotfiles-configuration
                           (directories '("./dots"))))
+                ;; (simple-service 'my-override
+                ;;                 home-shell-profile-service-type
+                ;;                 (list (local-file "config/profile-stub")))
                 (service home-files-service-type
                          `((".gitconfig" ,(local-file(cond ((equal? role "work") "config/git/gitconfig_work")
                                   (else "config/git/gitconfig"))))
+                           (".config/guix/zzz-guix.sh" ,(local-file "config/guix/zzz-guix.sh"))
                            (".config/waybar/scripts/cava.sh" ,(local-file "config/waybar-scripts/cava.sh" #:recursive? #t))
                            (".config/waybar/scripts/check_updates.sh" ,(local-file "config/waybar-scripts/check_updates.sh" #:recursive? #t))
                            (".config/waybar/scripts/openweathermap.sh" ,(local-file "config/waybar-scripts/openweathermap.sh" #:recursive? #t))
                            (".config/waybar/scripts/update_system.sh" ,(local-file "config/waybar-scripts/update_system.sh" #:recursive? #t))
                            (".config/waybar/scripts/vpn_status.sh" ,(local-file "config/waybar-scripts/vpn_status.sh" #:recursive? #t))
-                           (".config/sway/config.d/locker" ,(local-file(cond ((equal? role "work") "config/sway/gnome-locker")
-                                                           (else "config/sway/default-locker"))))                           
+                           (".config/sway/config.d/customs" ,(local-file(cond ((equal? role "work") "config/sway/work")
+                                                           (else "config/sway/personal"))))
+                           (".local/bin/start-sway" ,(local-file "config/bins/start-sway" #:recursive? #t))
+                           (".envrc" ,(local-file(cond ((equal? role "work") "config/home-envrc/envrc-work")
+                                                           (else "config/home-envrc/envrc-personal"))))
+                           (".face" ,(local-file(cond ((equal? role "work") "config/face/work.jpg")
+                                  (else "config/face/personal.jpg"))))
                            (".face.icon" ,(local-file(cond ((equal? role "work") "config/face/work.jpg")
-                                  (else "config/face/personal.jpg"))))))                
+                                  (else "config/face/personal.jpg"))))))
                 (service home-dbus-service-type)
-                (service home-ssh-agent-service-type)                
+                (service home-pipewire-service-type
+                         (home-pipewire-configuration
+                          (enable-pulseaudio? #t)))
+                (service home-ssh-agent-service-type)
                 (service home-openssh-service-type
                          (home-openssh-configuration
                           (add-keys-to-agent "yes"))))
